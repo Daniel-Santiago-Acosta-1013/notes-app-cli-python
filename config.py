115c9389825c7c388529661e0da499db6e0f399b
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-05-06 22:41:38 -0500

    [add] agregando env seguras

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-DATABASE_URI = 'postgresql://username:password@localhost/dbname'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+database = os.getenv('DB_NAME')
+
+DATABASE_URI = f"postgresql://{username}:{password}@{host}/{database}"
